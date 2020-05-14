1c828b1eb6bf00e108a5957f18e56455386b8ca7
Tawhidis007 <35430163+Tawhidis007@users.noreply.github.com>
2020-05-14 07:34:33 +0600

    uploading to github

---
+++ client/src/components/TextContainer/TextContainer.js
@@ -0,0 +1,38 @@
+﻿import React from 'react';
+
+import onlineIcon from '../../icons/onlineIcon.png';
+
+import './TextContainer.css';
+
+const TextContainer = ({ users }) => (
+    <div className="textContainer">
+        <div>
+            <h1>Chitter-Chatter <span role="img" aria-label="emoji">💬</span></h1>
+            <h3>Developed using React, Express, Node and Socket.IO</h3>
+            <h3>By Tawhid Shahrior</h3>
+          
+            
+        </div>
+        {
+            users
+                ? (
+                    <div>
+                        <h2>People in this room :</h2>
+                        <div className="activeContainer">
+                            <h2>
+                                {users.map(({ name }) => (
+                                    <div key={name} className="activeItem">
+                                        {name}
+                                        <img alt="Online Icon" src={onlineIcon} />
+                                    </div>
+                                ))}
+                            </h2>
+                        </div>
+                    </div>
+                )
+                : null
+        }
+    </div>
+);
+
+export default TextContainer;
